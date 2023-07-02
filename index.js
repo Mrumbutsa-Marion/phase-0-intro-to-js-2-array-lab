b9984f8d2271f1bf2b634f48d07d4445318012fc
Mrumbutsa-Marion <marion.mwanje@student.moringaschool.com>
2023-07-02 08:19:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+// beforeEach(function () {
+//     cats.length = 0;
+  
+//     cats.push("Milo", "Otis", "Garfield");
+//   });
+  function  destructivelyAppendCat(name){
+    cats.push(name);
+  }
+  function destructivelyPrependCat(name){
+    cats.unshift(name);
+  }
+  function destructivelyRemoveLastCat(){
+    cats.pop();
+  }
+  function   destructivelyRemoveFirstCat(){
+    cats.shift();
+  }
+  function   appendCat(name){
+    cats.push(name);
+  }
